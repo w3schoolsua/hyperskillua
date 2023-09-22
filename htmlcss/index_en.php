6d8bd347a73243892cdefc8c4edac55f685a0008
w3schoolsua <57772958+w3schoolsua@users.noreply.github.com>
2023-09-22 18:52:27 +0300

    add pages
    Signed-off-by: w3schoolsua <57772958+w3schoolsua@users.noreply.github.com>

---
+++ htmlcss/index_en.php
@@ -0,0 +1,119 @@
+<?php include '../include/head_en.php'; ?>
+<title>Tutorials from Hyperskill. Why do you need the Hyperskill? <?php include '../include/title_en.php'; ?></title>
+<meta name='description' content='Tutorials from Hyperskill site. What is Hyperskill? Who owns Hyperskill? What is JetBrains? What does she do and what does she produce? Who is recommended to use Hyperskill? What can be learned at Hyperskill? What is the traffic of the Hyperskill site? What are the features of training at Hyperskill? Correct answers to tests (questions). <?php include '../include/description_en.php'; ?>'>
+<?php include '../include/topmenu_en.php'; ?>
+<?php include '../include/leftmenu_html_hyperskill_en.php'; ?>
+<?php include '../include/before_content_html_en.php'; ?>
+<p class='w3-right'>
+    <a href='index.php' title='Go to Ukrainian language'>Ua</a>
+</p>
+<article>
+    <h1>Programming tutorials <span class='color_h1'>from Hyperskill. Start of training</span></h1>
+    <div class='w3-clear nextprev'>
+        <a class='w3-left w3-btn' href='../index_en.php'>&#10094; Home</a>
+        <a class='w3-right w3-btn' href='computer-programming_en.php'>Next &#10095;</a>
+    </div>
+    <hr>
+    <h2>What is Hyperskill?</h2>
+    <p><strong>Hyperskill</strong> is an online platform for learning tech skills. It offers a variety of courses on different programming languages, web development, data science, and other topics. The courses are project-based, which means that you will learn by working on real-world projects.</p>
+    <p>Here are some of the reasons why you might need the site <a href="https://hyperskill.org/" target="_blank" rel="nofollow">https://hyperskill.org/</a>:</p>
+    <ul>
+        <li>You want to learn a new programming language or technology.</li>
+        <li>You want to improve your coding skills.</li>
+        <li>You want to get a job with the tech industry.</li>
+        <li>You are a self-taught developer and want to learn from experts.</li>
+        <li>You are a student or teacher who wants to use a project-based learning platform.</li>
+    </ul>
+    <hr>
+    <h2>Who owns Hyperskill?</h2>
+    <p><strong>Hyperskill</strong> is owned by <a href="https://www.jetbrains.com/" target="_blank" rel="nofollow"><strong>JetBrains</strong></a>, a company that develops IDEs and other development tools. The author of the site and the materials on it are a team of experienced developers and educators.</p>
+    <hr>
+    <h2>What is JetBrains? What does she do and what does she produce?</h2>
+    <p><strong>JetBrains</strong> is a Czech multinational software development company that develops integrated development environments (IDEs) and other development tools. The company was founded in 2000 and is headquartered in Prague, Czech Republic.</p>
+    <p>JetBrains' IDEs are used by millions of developers around the world. Some of the most popular JetBrains IDEs include:</p>
+    <ul>
+        <li><strong>IntelliJ IDEA</strong>: An IDE for Java, Kotlin, and other JVM languages.</li>
+        <li><strong>PyCharm</strong>: An IDE for Python.</li>
+        <li><strong>WebStorm</strong>: An IDE for JavaScript and related technologies.</li>
+        <li><strong>PhpStorm</strong>: An IDE for PHP.</li>
+        <li><strong>Rider</strong>: An IDE for .NET and Mono.</li>
+        <li><strong>RubyMine</strong>: An IDE for Ruby.</li>
+        <li><strong>CLion</strong>: An IDE for C and C++.</li>
+        <li><strong>GoLand</strong>: An IDE for Go.</li>
+        <li><strong>DataGrip</strong>: An IDE for SQL.</li>
+        <li><strong>AppCode</strong>: An IDE for macOS and iOS development.</li>
+    </ul>
+    <p>In addition to IDEs, JetBrains also develops a variety of other development tools, such as:</p>
+    <ul>
+        <li><strong>TeamCity</strong>: A continuous integration and delivery server.</li>
+        <li><strong>YouTrack</strong>: A bug tracking and issue tracking system.</li>
+        <li><strong>Space</strong>: A code collaboration platform.</li>
+        <li><strong>Toolbox App</strong>: A desktop app that provides access to all of JetBrains' tools.</li>
+    </ul>
+    <p>JetBrains also offers a variety of educational resources, such as:</p>
+    <ul>
+        <li><strong>Hyperskill</strong>: An online platform for learning tech skills.</li>
+        <li><strong>Academy</strong>: A collection of online courses on different programming languages and technologies.</li>
+        <li><strong>Blog</strong>: A blog that covers news and tips on software development.</li>
+    </ul>
+    <p>JetBrains is a leading provider of development tools and educational resources for software developers. The company's products are used by millions of developers around the world, and its educational resources help developers learn new skills and stay up-to-date on the latest technologies.</p>
+    <hr>
+    <?php include '../include/adinfeed.php'; ?>
+    <h2>Who is recommended to use Hyperskill?</h2>
+    <p>The site is recommended for anyone who wants to learn tech skills. It is especially suitable for beginners and self-taught developers. The courses are well-structured and easy to follow, and the projects are challenging but rewarding.</p>
+    <hr>
+    <h2>What can be learned at Hyperskill?</h2>
+    <p>You can learn a variety of things on Hyperskill, including:</p>
+    <ul>
+        <li>Programming languages: Java, Python, JavaScript, C++, Kotlin, and more.</li>
+        <li>Web development: HTML, CSS, JavaScript, React, Angular, and more.</li>
+        <li>Data science: machine learning, statistics, and data analysis.</li>
+        <li>DevOps: continuous integration and delivery, cloud computing, and more.</li>
+        <li>And much more!..</li>
+    </ul>
+    <hr>
+    <h2>What is the traffic of the Hyperskill site?</h2>
+    <p>The traffic of the site is over 1 million users per month.</p>
+    <hr>
+    <h2>What are the features of training at Hyperskill?</h2>
+    <p>Here are some features of Hyperskill:</p>
+    <ul>
+        <li>Project-based learning: You learn by working on real-world projects.</li>
+        <li>Personalized study plan: The platform adapts to your learning style and pace.</li>
+        <li>Integration with JetBrains IDEs: You can code directly in the IDE.</li>
+        <li>Community of learners: You can ask questions and get help from other learners.</li>
+        <li>Certificate of completion: You can earn a certificate of completion for each course you complete.</li>
+    </ul>
+    <p>If you are interested in learning tech skills, recommend checking out Hyperskill. It is a great platform for anyone who wants to learn by doing.</p>
+    <hr>
+    <h2>Where can you find the correct answers to the tasks from the website Hyperskill?</h2>
+    <p>There are a few places where you can find the correct answers to the tasks from the website https://hyperskill.org/:</p>
+    <ul>
+        <li>In the task itself: In some tasks, the correct answers are provided directly in the task. For example, in the task "Determine if a number is even", the correct answer is provided as a line of code that returns true or false depending on whether the number is even.</li>
+        <li>In the theory: The theory for each task provides an explanation that includes the relevant rules or algorithms. For example, the theory for the task "Determine if a number is even" explains how to determine the parity of a number.</li>
+        <li>In the comments for the task: In the comments for the task, users can share their solutions and correct answers. However, it is important to remember that not all comments are correct.</li>
+        <li>In the Hyperskill community: The Hyperskill community is an online forum where users can discuss tasks, share their solutions, and help each other. In the community, you can find many correct answers to tasks.</li>
+    </ul>
+    <p>Here are some specific tips for finding the correct answers to tasks from the Hyperskill website:</p>
+    <ul>
+        <li>Try to solve the task yourself first. If you cannot solve the task yourself, refer to the theory or comments.</li>
+        <li>Check the correctness of the answers in multiple sources. Do not trust a single answer that you found in the comments. Check the correctness of the answer in the theory or Hyperskill community.</li>
+        <li>Do not hesitate to ask for help from the Hyperskill community. There are many experienced users in the community who can help you solve the task.</li>
+        <li>Use the W3SchoolsEN website to find out the correct answer to the Hyperskill task.</li>
+    </ul>
+    <hr>
+    <h2>What do we offer? Correct answers to tests (questions)!</h2>
+    <p>We bring to your attention some lessons from the popular site <a href="https://hyperskill.org/" target="_blank" rel="nofollow">hyperskill.org</a> with ready-made answers to tasks.</p>
+    <p>This tutorial covers some general programming topics. Lessons on specific programming languages and areas are also gradually added to the site. You can choose lesson topics from the menu on the left (or by using the burger menu if you use mobile devices).</p>
+    <p>Also, we offer you ready-made answers to each lesson on the Hyperskill website with detailed explanations on how to solve each of the tasks. For English-speaking users, all lessons are taught in English, but if necessary, you can also view all lessons in Ukrainian translation by clicking on the <a href="index.php"><b>Ua</b></a> link (in the upper right corner) on each lesson page, or by visiting the lesson directly at site <a href="https://hyperskill.org/" target="_blank" rel="nofollow">hyperskill.org</a>.</p>
+    <p class="w3-warning"><b>Note.</b> ATTENTION! The site is currently only being developed and is constantly being filled with new lessons. Therefore, some links (lesson topics) on the site may temporarily not work!</p>
+    <hr>
+    <div class='w3-clear nextprev'>
+        <a class='w3-left w3-btn' href='../index_en.php'>&#10094; Home</a>
+        <a class='w3-right w3-btn' href='computer-programming_en.php'>Next &#10095;</a>
+    </div>
+</article>
+<?php include '../include/addown_content.php'; ?>
+</div>
+<?php include '../include/rightbar_html_css_js_en.php'; ?>
+<?php include '../include/footer_en.php'; ?>
